eaecb9cf9d35c63351d5ab6c9b7ee961a411955b
naveen yadav <navyad.pro@gmail.com>
2016-08-24 19:31:17 +0530

    PEP-8 errors removed

--- sqlwrapper.py
+++ sqlwrapper.py
@@ -3,27 +3,35 @@ Sample file for trying sqlite binding from SQLObject
 
 http://www.sqlobject.org/
 """
-
-import os 
-from sqlobject import *
+import os
+from sqlobject import (
+    SQLObject, StringCol, IntCol, ForeignKey,
+    connectionForURI, sqlhub)
 
 
 class Response(SQLObject):
+    """
+    Represents table in Sqlite
+    """
     text = StringCol()
     occurence = IntCol()
 
+
 class Statement(SQLObject):
+    """
+    Represents table in database
+    """
     text = StringCol()
     response = ForeignKey('Response')
 
 
-def database_file_name():
+def file_path(file_name):
     current_dir_path = os.path.dirname(os.path.realpath(__file__))
     directory_sep = os.path.sep
-    return directory_sep.join([current_dir_path, "library.db"])
-    
+    return directory_sep.join([current_dir_path, file_name])
 
-def connection(file_name):
+
+def create_connection(file_name):
     if os.path.exists(file_name):
         os.unlink(file_name)
     _scheme = 'sqlite:' + file_name
@@ -32,10 +40,10 @@ def connection(file_name):
     return conn
 
 if __name__ == '__main__':
-    file_name = database_file_name()
-    connection = connection(file_name)
+    _path = file_path("library.db")
+    connection = create_connection(_path)
 
-    #create table
+    # create table
     Response.createTable()
 
     # add records to table
@@ -46,7 +54,7 @@ if __name__ == '__main__':
 
     # update a record
     response_2.set(text="Bye")
-    
+
     # fetch single item
     print Response.get(1)
 
