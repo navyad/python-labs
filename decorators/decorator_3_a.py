7a945b1adbe04cfed9320ca7d26f193b1d1adc0c
naveen yadav <navyad.pro@gmail.com>
2016-08-25 22:29:56 +0530

    decorators example moved to directory

---
+++ decorators/decorator_3_a.py
@@ -0,0 +1,16 @@
+def outer(some_func):
+    def inner(a, b):
+        return some_func(a, b)
+    inner.__name__ = some_func.__name__
+    inner.__doc__ = some_func.__doc__
+    return inner
+
+
+@outer
+def add(x, y):
+    """adds a x and y"""
+    return x + y
+
+if __name__ == '__main__':
+    print add.__name__ # add
+
