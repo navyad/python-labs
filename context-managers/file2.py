a7f58618fd9562ac14df23aa97e45be789e43da3
navyad <navyad.pro@gmail.com>
2018-10-16 13:08:54 +0530

    context manager examples

--- context-managers/file2.py
+++ context-managers/file2.py
@@ -1,4 +1,4 @@
-from contextlib import AbstractContextManager
+from contextlib import AbstractContextManager, ContextDecorator
 
 
 class TaxiDriverManager(AbstractContextManager):
@@ -12,6 +12,7 @@ class TaxiDriverManager(AbstractContextManager):
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         print("**** exiting ****")
+        return False
 
     def mirror_scene(self, dialogue):
         if dialogue != self.dialogue:
@@ -19,9 +20,39 @@ class TaxiDriverManager(AbstractContextManager):
         print(dialogue)
 
 
-#ErrorManager().call_me()
-
 with TaxiDriverManager() as obj:
     random_dialogue = "this and that"
-    #obj.mirror_scene(obj.dialogue)
-    obj.mirror_scene(random_dialogue)
+    #obj.mirror_scene(random_dialogue)
+
+
+
+
+
+"""
+ContextDecorator:
+    1. can be used as a decorator
+    2. can be used 'with' statement
+"""
+class director(ContextDecorator):
+
+    def __enter__(self):
+        print("**** director: enterting ****")
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print("**** director: exiting ****")
+        return False
+
+
+
+# #: Use: 1
+@director()
+def movie_director():
+    print("director: martin")
+
+movie_director()
+
+
+#Use: 2
+with director() as dir:
+    print("director: martin")
